ba2100aca7a400e48ce3df9c510e05fce312a779
bradc1993 <bradc1993@gmail.com>
2020-07-15 01:29:09 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const list = document.getElementById('tasks');
+  const taskForm = document.querySelector('form#create-task-form');
+  taskForm.addEventListener('submit', (event) => {
+    let listElement = document.createElement('li');
+    let input = taskForm.elements[0];    
+    listElement.innerText = input.value;
+
+    list.appendChild(listElement);
+
+    event.preventDefault();
+    }
+  )
 });
